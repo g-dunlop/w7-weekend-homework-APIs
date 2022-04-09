0d8a6bb8c76cf7f5a4f4d144ee8450a05df9ff7d
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-04-09 13:01:22 +0100

    page up and down working

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Article from './Article'
 
-const ArticleList = ({articles, allArticles}) => { 
+const ArticleList = ({articles, allArticles, turnPage}) => { 
 
+    const handleClick = (evt) => {
+        console.log(evt.target.value)
+        turnPage(evt.target.value)
+
+    }
 
     const articleNodes = articles.map((article, index) => {
         return <Article article={article} key={index}/>
@@ -13,8 +18,8 @@ const ArticleList = ({articles, allArticles}) => {
             <h2>searchResults</h2>
             <p>Articles matching your search: {allArticles.total}</p>
             <p> Page {allArticles.currentPage} of {allArticles.pages}
-            <button value="lower" >Previous Page</button>
-            <button value="higher" >Next Page</button>
+            <button onClick={handleClick} value="lower" >Previous Page</button>
+            <button onClick={handleClick} value="higher" >Next Page</button>
             </p>
 
 
--- src/containers/GrauniadContainer.js
+++ src/containers/GrauniadContainer.js
@@ -8,6 +8,8 @@ const GrauniadContainer = () => {
     const [apiUrl, setApiUrl] = useState('https://content.guardianapis.com/search?page=1&q=&api-key=test');
     const [searchResults, setSearchResults] = useState([]);
     const [allSearchResults, setAllSearchResults] = useState([]);
+    const [searched, setSearched] = useState("");
+    
 
     useEffect(() => {
        getNews() 
@@ -31,14 +33,25 @@ const GrauniadContainer = () => {
         let newUrl = `https://content.guardianapis.com/search?page=1&q=${searchTerm}&api-key=test`
         setApiUrl(newUrl)
         console.log(newUrl)
+        setSearched(searchTerm)
+        
     }
 
+    const turnPage = (upOrDown) => {
+        let page;
+        if (upOrDown === 'higher'){
+            page = (allSearchResults.currentPage+1)
+        } else (
+            page = (allSearchResults.currentPage-1)
+        ) 
+        setApiUrl(`https://content.guardianapis.com/search?page=${page}&q=${searched}&api-key=test`)
+    }
 
     return (
         <div>
             <h2>The GrauniadContainer</h2>
             <SearchBar searchApi={searchApi}/>
-            <ArticleList articles = {searchResults} allArticles={allSearchResults} />
+            <ArticleList articles = {searchResults} allArticles={allSearchResults} turnPage={turnPage} />
         </div>
     )
 
